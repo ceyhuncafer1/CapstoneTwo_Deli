acb172c6c70a255b846037d08b929711fc83349d
Ceyhun Cafer <ceyhuncafer1@gmail.com>
2024-05-28 10:44:26 -0400

    Order implementation

--- src/main/java/com/ps/Order.java
+++ src/main/java/com/ps/Order.java
@@ -1,4 +1,24 @@
 package com.ps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    private List<Product> products;
+
+    public Order(){
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "products=" + products +
+                '}';
+    }
 }
