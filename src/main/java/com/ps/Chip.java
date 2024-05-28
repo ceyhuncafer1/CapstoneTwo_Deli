81cc1b8576b607370e65d25880a84b812649f1fa
Ceyhun Cafer <ceyhuncafer1@gmail.com>
2024-05-28 10:28:14 -0400

    Chip added

--- src/main/java/com/ps/Chip.java
+++ src/main/java/com/ps/Chip.java
@@ -1,4 +1,23 @@
 package com.ps;
 
 public class Chip extends Product{
+
+    private String chipType;
+
+    public Chip(String chipType){
+        this.chipType = chipType;
+    }
+
+    @Override
+    public double calcPrice() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Chip{" +
+                "chipType='" + chipType + '\'' +
+                '}';
+    }
+
 }
--- src/main/java/com/ps/Drink.java
+++ src/main/java/com/ps/Drink.java
@@ -1,4 +1,15 @@
 package com.ps;
 
 public class Drink extends Product{
+
+
+    @Override
+    public double calcPrice() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
