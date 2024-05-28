b224426463bc4e15b20a3673846fcbbe729650bd
Ceyhun Cafer <ceyhuncafer1@gmail.com>
2024-05-28 11:43:54 -0400

    Display order screen created

--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class UserInterface {
 
     private Scanner scanner;
-    int userMenuChoice;
+    int userHomeScreenChoice;
+    int userOrderScreenChoice;
 
     public UserInterface(){
         scanner = new Scanner(System.in);
@@ -17,15 +18,57 @@ public class UserInterface {
 
             do{
 
+                System.out.println("Pick an option");
                 System.out.println("1) New Order");
                 System.out.println("0) Exit Deli App");
 
-            } while(userMenuChoice != 0);
+                userHomeScreenChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                if(userHomeScreenChoice == 1){
+                    //displayOrderScreen();
+                }
+
+
+
+            } while(userHomeScreenChoice != 0);
 
         } catch(Exception e){
             System.out.println("Error");
         }
 
     }
+
+    public void displayOrderScreen(){
+
+        try{
+
+            do{
+
+                System.out.println("1) Add Sandwich");
+                System.out.println("2) Add Drink");
+                System.out.println("3) Add Chips");
+                System.out.println("4) Checkout");
+                System.out.println("0) Cancel Order");
+
+                userOrderScreenChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch(userOrderScreenChoice){
+                    case 1:
+                        // Goal is to make an instance of THIS sandwich for THIS order for THIS product. Need to instantiate a new order.
+                }
+
+
+
+            } while(userOrderScreenChoice != 0);
+
+
+        } catch(Exception e){
+            System.out.println("Error");
+        }
+
+    }
+
 
 }
