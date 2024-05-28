c1d64b0e603692a41b4f65e7f6b2849be72a5b03
Ceyhun Cafer <ceyhuncafer1@gmail.com>
2024-05-28 11:06:49 -0400

    Early ui implementation

--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -1,4 +1,31 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class UserInterface {
+
+    private Scanner scanner;
+    int userMenuChoice;
+
+    public UserInterface(){
+        scanner = new Scanner(System.in);
+    }
+
+    public void displayHomeScreen(){
+
+        try{
+
+            do{
+
+                System.out.println("1) New Order");
+                System.out.println("0) Exit Deli App");
+
+            } while(userMenuChoice != 0);
+
+        } catch(Exception e){
+            System.out.println("Error");
+        }
+
+    }
+
 }
